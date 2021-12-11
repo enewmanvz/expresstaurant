a6bba5f94f9439ac72daaba1ab8d9a5161e4b050
Newman <newmer8@ad.vzwcorp.com>
2021-12-10 23:39:45 -0600

    commit updates to index.js comment--associations

--- models/index.js
+++ models/index.js
@@ -6,15 +6,10 @@ const { Item } = require('.models/Item')
 
 //associate models
 //adds foreign key to musician table connecting a musician instance to a specific band
-
 Menu.belongsTo(Restaurant)
-
 //gives us sequelize methods for a one to many relationship
-
 Restaurant.hasMany(Menu)
-
 Item.belongsTo(Menu)
-
 Menu.hasMany(Item)
 
 //export models with added associations
